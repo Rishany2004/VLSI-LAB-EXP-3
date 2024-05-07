3d66bd56eb65b1df7699a010fb9b69e5bf38ea90
Rishany2004 <159290227+Rishany2004@users.noreply.github.com>
2024-05-07 17:54:04 +0530

    Update Exp3_Multiplier_2bit.v

--- Exp3_Multiplier_2bit.v
+++ Exp3_Multiplier_2bit.v
@@ -1,12 +1,17 @@
 module ha(a,b,sum,carry);
 input a,b;
 output sum,carry;
-
+assign sum=a^b;
+assign carry=a&b;
 endmodule
-
-module multi_2(a,b,p,carry);
-input [1:0]a,b;
-output [2:0]p;
-output carry;
-
+module mux2bit(A,B,C);
+input [1:0]A,B;
+output [3:0]C;
+wire W1,W2,W3,W4;
+and g1(C[0],A[0],B[0]);
+and g2(W1,A[0],B[1]);
+and g3(W2,A[1],B[0]);
+and g4(W3,A[1],B[1]);
+ha ha1(W1,W2,C[1],W4);
+ha ha2(W4,W3,C[3],C[2]);
 endmodule
